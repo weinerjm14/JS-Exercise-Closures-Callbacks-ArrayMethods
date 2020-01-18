950a9068e227dd17c311712c93b7e37342b06ef5
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-01-17 21:50:02 -0500

    Completed Stretch

--- index.js
+++ index.js
@@ -159,8 +159,8 @@ function processContains(x, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  return callback(Array.from(new Set(list)));
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
